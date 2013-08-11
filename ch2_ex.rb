a6a6ac7773dce9a1083b9aba9ae2011c08e9f650
Gina Zhou <ginayizhou@gmail.com>
2013-08-11 16:44:31 -0700

    first commit

---
+++ ch2_ex.rb
@@ -0,0 +1,11 @@
+# This is how many hours are in a year.
+puts 365 * 24
+
+# This is how many minutes are in a decade.
+puts 10 * 365 * 24 * 60
+
+# My age in seconds.
+puts 23 * 365 * 24 * 60 * 60
+
+# Finding the author's age if he is 1,160 million seconds old.
+puts 1160000000 / (60 * 60 * 24 * 365)
